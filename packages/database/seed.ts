ab1f54b41d27d9ac077737187fd7f52663003cc7
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-04-06 00:56:13 +0200

    feet: seet

--- packages/database/seed.ts
+++ packages/database/seed.ts
@@ -1,9 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Polygon } from "@prisma/client";
 import { faker } from "@faker-js/faker";
+import data from "../../apps/web/minified.json";
 
 const database = new PrismaClient();
 async function seed() {
   console.log(`Successfully seeded database 🌱`);
+
+  // @ts-ignore
+  const polygons: Polygon[] = data.features.map((feature) => ({
+    Id: feature.properties.polygon_id,
+    TreesLikeCount: 0,
+    FlowersLikeCount: 0,
+    DrinkFountainLikeCount: 0,
+    PublicToiletLikeCount: 0,
+  }));
+
+  await database.polygon.createMany({
+    data: polygons,
+    skipDuplicates: true,
+  });
 }
 
 seed();
