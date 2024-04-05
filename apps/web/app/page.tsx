e373b5229e47b8910e8e27ff9e91347ed66e54d2
Phisch <phischdev@users.noreply.github.com>
2024-04-05 23:55:54 +0200

    Co-authored-by: vlle1 <vlle1@users.noreply.github.com>

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Map, { Spot } from "@/components/map";
+import WayDetails from "@/components/WayDetails";
 import { useState } from "react";
 
 export default function Page() {
@@ -11,6 +12,9 @@ export default function Page() {
         <Map setSelectedSpot={setSelectedSpot} />
       </div>
       <div className="absolute right-0 h-screen w-96 rounded-l-lg bg-white p-4">
+        {selectedSpot && (
+          <WayDetails />
+        )}
         <h1 className="text-4xl font-medium">Modules</h1>
         <pre>{JSON.stringify(selectedSpot, null, 2)}</pre>
       </div>
---
+++ apps/web/components/CityModule.tsx
@@ -0,0 +1,38 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "ui/components/ui/card";
+
+import {
+    Button
+} from "ui/components/ui/button";
+
+import { Heart } from "lucide-react";
+
+export default function CityModule({ name, Icon, description = null, likeCount, isLiked = false }) {
+    return (
+        <Card className="shadow-md hover:shadow-lg hover:bg-gray-100 cursor-pointer">
+            <CardHeader>
+                <CardTitle className="text-lg">
+                    <div className="flex justify-between">
+                        <div className="flex gap-2">
+                            {Icon && <Icon className="h-6 w-6 relative top-[2px]" />}
+                            {name}
+                        </div>
+                        <div className="flex gap-2">
+                            <Heart fill={isLiked ? "red" : "none"} className="h-6 w-6 top-[-5px]" />
+                            {likeCount + ' '}
+                        </div>
+                    </div>
+
+                </CardTitle>
+            </CardHeader>
+            {description && <CardContent>{description}</CardContent>
+            }
+        </Card >
+    );
+}
