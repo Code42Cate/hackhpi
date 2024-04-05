4ceadae4882553026b97b820047cc6e6e75280c6
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-04-05 18:09:03 +0200

    feat: wip

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
 import Map from "@/components/map";
+import { useState } from "react";
 
 export default function Page() {
+  const [selectedSpot, setSelectedSpot] = useState<string | null>(null);
+
   return (
     <div className="relative flex h-screen flex-col items-center justify-center">
       <div className="w-full shadow-sm">
-        <Map />
+        <Map setSelectedSpot={setSelectedSpot} />
       </div>
       <div className="absolute right-0 h-screen w-96 rounded-l-lg bg-white p-4">
         <h1 className="text-4xl font-medium">Modules</h1>
+        {selectedSpot}
       </div>
     </div>
   );
--- apps/web/components/map.tsx
+++ apps/web/components/map.tsx
@@ -8,20 +8,21 @@ import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 mapboxgl.accessToken =
   "pk.eyJ1IjoiY29kZTQyY2F0ZSIsImEiOiJjbHU5MG15NzEwNGJpMmpta2gzNWMxazFqIn0.DmdGNtsf_SCeRxqEDlt0UQ";
 
-export default function Map() {
+export default function Map({
+  setSelectedSpot,
+}: {
+  setSelectedSpot: (spot: string) => void;
+}) {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(13.449106180576361);
-  const [lat, setLat] = useState(52.51349867057073);
-  const [zoom, setZoom] = useState(16);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/standard",
-      center: [lng, lat],
-      zoom: zoom,
+      center: [13.4394958, 52.518519],
+      zoom: 18,
       antialias: true,
       dragRotate: true,
       dragPan: true,
@@ -37,7 +38,7 @@ export default function Map() {
     map.current.addControl(Draw, "top-left");
 
     map.current.on("load", () => {
-      map.current.addSource("parkingspot0", {
+      map.current.addSource("parkingspots", {
         type: "geojson",
         data: {
           type: "Feature",
@@ -45,11 +46,11 @@ export default function Map() {
             type: "Polygon",
             coordinates: [
               [
-                [13.448435173048354, 52.51399535732365],
-                [13.448388420543562, 52.514022845002046],
-                [13.448336913371245, 52.51397655009469],
-                [13.448393967273294, 52.51395340257737],
-                [13.448435173048354, 52.51399535732365],
+                [13.4394958, 52.518519],
+                [13.4394149, 52.5185354],
+                [13.439405, 52.5185173],
+                [13.4394859, 52.5185009],
+                [13.4394958, 52.518519],
               ],
             ],
           },
@@ -58,9 +59,9 @@ export default function Map() {
 
       // Add a new layer to visualize the polygon.
       map.current.addLayer({
-        id: "parkingspot0",
+        id: "parkingspots",
         type: "fill",
-        source: "parkingspot0", // reference the data source
+        source: "parkingspots", // reference the data source
         layout: {},
         paint: {
           "fill-color": "#0080ff", // blue color fill
@@ -71,13 +72,27 @@ export default function Map() {
       map.current.addLayer({
         id: "outline",
         type: "line",
-        source: "parkingspot0",
+        source: "parkingspots",
         layout: {},
         paint: {
           "line-color": "#0080ff",
           "line-width": 2,
           "line-opacity": 0.8,
         },
+      });
+
+      map.current.on("mouseenter", "parkingspots", () => {
+        map.current.getCanvas().style.cursor = "pointer";
+      });
+
+      // Change the cursor back to a pointer
+      // when it leaves the states layer.
+      map.current.on("mouseleave", "parkingspots", () => {
+        map.current.getCanvas().style.cursor = "";
+      });
+
+      map.current.on("click", "parkingspots", (e) => {
+        setSelectedSpot(e.lngLat.toString());
       });
     });
   });
